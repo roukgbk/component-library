36611146e492d4bdef15d2f7c587d9a39d4f0fe9
liuyihong <15033925941@163.com>
2023-07-11 16:01:17 +0800

    feat：增加默认sass样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './styles/index.scss'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.querySelector('#root')!).render(
+  <React.Suspense>
     <App/>
-  </React.StrictMode>,
+  </React.Suspense>
 )
